960a82ea35f75617abf8c00b477f066ab975cfb2
jina26 <67463648+jina26@users.noreply.github.com>
2021-03-23 16:12:38 +0900

    no message

--- static/js/solution.js
+++ static/js/solution.js
@@ -1,16 +1,10 @@
-// 로딩 후 바로 실행
-
 if ($.cookie('mytoken') == undefined) {
-    // mytoken이라는 값으로 쿠키가 없으면, 로그인 창으로 이동시킵니다.
     alert('먼저 로그인을 해주세요')
     window.location.href = '/login'
 } else {
-    // 쿠기가 있으면, 유저 정보를 불러옵니다.
     load_user_info()
 }
 
-
-// 쿠키에 가지고 있는 token을 헤더에 담아서 보냅니다.
 function load_user_info() {
     $.ajax({
         type: "GET",
